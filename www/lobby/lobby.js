36cc14f7857b2ccc6ad3e7cb3fd567347122c824
Sebastian Fuenmayor <fuenmayorsebastiian@gmail.com>
2025-09-09 13:34:13 -0300

    update[homePage/,lobby/, settings.css] Cambios mínimos a settings.css para que aproveche más a general.css homePage tiene arreglos al "popup" para que quede más estético, junto a una mejora estética del input dentro de playSection Hubieron cambios en lobby/ para aprovechar de general.css, ahora contamos con un diseño responsive que se ve bien en la mayoría de pantallas

---
+++ www/lobby/lobby.js
@@ -0,0 +1,20 @@
+const backBtn = document.getElementById("backBtn");
+const joinBtn = document.getElementById("joinBtn");
+const startBtn = document.getElementById("startBtn");
+
+backBtn.addEventListener("click", () =>{
+    window.location.href = "../../homePage/home.html"
+})
+
+if(joinBtn){
+    joinBtn.addEventListener("click", () =>{
+        window.location.href = "../game/gameGuest/game.html"
+    })
+}
+
+if(startBtn){
+    startBtn.addEventListener("click", () =>{
+        window.location = "../../game/gameHost/game.html"
+    })
+}
+
