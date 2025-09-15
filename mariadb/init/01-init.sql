0dffdeb0b92946cd035bc66a7c6cd10d8a1137a4
arielpajares <arielpajaresf@gmail.com>
2025-09-14 22:26:45 -0300

    src-delete-[DIR]
    Eliminamos el volumen de mariadb data e init

--- mariadb/init/01-init.sql
+++
@@ -1,6 +0,0 @@
--- Crear usuario gbloomer con permisos desde cualquier host
-CREATE USER IF NOT EXISTS 'gbloomer'@'%' IDENTIFIED BY 'goldenblosser';
-GRANT select, insert, delete, update ON gbloom_db.* TO 'gbloomer'@'%';
-GRANT ALL PRIVILEGES ON gbloom_db.* TO 'gbloomer'@'localhost';
-
-FLUSH PRIVILEGES;
