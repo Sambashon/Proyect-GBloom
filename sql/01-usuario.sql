de059b155d2a22e7598c06807dc91b1bd6ef4b1c
root <root@T140J2312080483.localdomain>
2025-10-07 17:12:35 -0300

    Arreglo de Error del entrypoint de docker al crear la base de datos

--- sql/01-usuario.sql
+++ sql/01-usuario.sql
@@ -1,5 +1,6 @@
 -- Crear usuario gbloomer con permisos desde cualquier host
 CREATE USER IF NOT EXISTS 'gbloomer'@'%' IDENTIFIED BY 'goldenblosser';
+CREATE USER IF NOT EXISTS 'gbloomer'@'localhost' IDENTIFIED BY 'goldenblosser';
 GRANT select, insert, delete, update ON gbloom_db.* TO 'gbloomer'@'%';
 GRANT ALL PRIVILEGES ON gbloom_db.* TO 'gbloomer'@'localhost';
 
