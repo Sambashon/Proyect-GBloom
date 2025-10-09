02126fe2a812cb4327a09594a1992fdc1e92b59b
nachovazquez12007@gmail.com <GBloomWSL@T140J2401082406.localdomain>
2025-10-09 09:34:03 -0300

    En este commit se ha arreglado un bug menor en credentials.php

--- www/php/scripts/utilities/credentials.php
+++ www/php/scripts/utilities/credentials.php
@@ -3,7 +3,7 @@ include "../../clases/perfil.php";
 
 $perfil = new Perfil();
 try {
-    $token = $_COOKIE["golden-token"];
+    $token = isset($_COOKIE["golden-token"]) ? $_COOKIE["golden-token"] : "";
     $credentials = $perfil->getUserCredentials($token);
 
     echo json_encode($perfil->returnSuccess($credentials["result"]));
